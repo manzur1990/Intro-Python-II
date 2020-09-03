507bb24f82049c66613d9955b531173885609efd
JM <manzur-1990@hotmail.com>
2020-09-03 17:46:26 -0500

    Day 2 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,25 @@
 from room import Room
 from player import Player
+from items import Item
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", Item('bow')),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", Item('helmet')),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", Item('sword')),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", Item('shield')),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", Item('empty chest')),
 }
 
 
@@ -38,8 +39,11 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player("Barthimus", room['outside'])
+player_name = input("What is your name?")
 
+player = Player(player_name, room['outside'])
+
+print(f"Welcome {player.name}!")
 
 # Write a loop that:
 #
@@ -53,37 +57,61 @@ player = Player("Barthimus", room['outside'])
 # If the user enters "q", quit the game.
 
 while True:
+    current_room = player.room
+    player_inventory = [x.name for x in player.inventory] if len(
+        player.inventory) else "Empty"
+    room_inventory = [x.name for x in current_room.item] if len(
+        current_room.item) else "Empty Room"
+
     print(
-        player.room.name, "\n",
-        player.room.description
-    )
+        f" === Entered {player.room.room_name}, {current_room.room_description} ===")
+    print(f"*You have found a {room_inventory}")
 
-    direction = input("Input a direction: ")
+    print(f"Inventory: [{len(player.inventory)}]")
+    print(f"Bag: {player_inventory}")
 
-    if direction.lower() == 'q':
-        print(f"Game Ended")
-        break
+    move = input(
+        '[Movement]: [N]/[S]/[W]/[E]. [Take/Drop]: [T]/[D]. [Q]-Quit \n :> ')
 
-    elif direction.lower() == 'n':
-        if hasattr(player.room, "n_to"):
-            player.room = player.room.n_to
+    action = move[0:4]
+    item = move[5:]
+
+    if move.lower() == 'n':
+        if current_room.n_to is not None:
+            player.room = current_room.n_to
+            print(f"\t==> Moving north")
         else:
-            print(f"Wrong direction.")
-
-    elif direction.lower() == 's':
-        if hasattr(player.room, "s_to"):
-            player.room = player.room.s_to
+            print("XXX Can't go that way XXX")
+    elif move.lower() == 's':
+        if current_room.s_to is not None:
+            player.room = current_room.s_to
+            print(f"\t==> Moving south")
         else:
-            print(f"Wrong direction.")
-
-    elif direction.lower() == 'w':
-        if hasattr(player.room, "w_to"):
-            player.room = player.room.w_to
+            print("XXX Can't go that way XXX")
+    elif move.lower() == 'e':
+        if current_room.e_to is not None:
+            player.room = current_room.e_to
+            print(f"\t==> Moving east")
         else:
-            print(f"Wrong direction.")
-
-    elif direction.lower() == 'e':
-        if hasattr(player.room, "e_to"):
-            player.room = player.room.e_to
+            print("XXX Can't go that way XXX")
+    elif move.lower() == 'w':
+        if current_room.w_to is not None:
+            player.room = current_room.w_to
+            print(f"\t==> Moving West")
         else:
-            print(f"Wrong direction.")
+            print("XXX Can't go that way XXX")
+
+    elif action.lower() == 'take':
+        item_name_list = [i.name for i in current_room.item]
+        item_index = item_name_list.index(item)
+        player.take(current_room.item[item_index])
+        current_room.remove_item(item_index)
+
+    elif action.lower() == 'd':
+        item_name_list = [i.name for i in player.inventory]
+        item_index = item_name_list.index(item)
+        current_room.add_item(player.inventory[item_index])
+        player.drop(item_index)
+
+    elif move == 'q':
+        exit()
--- src/room.py
+++ src/room.py
@@ -2,8 +2,24 @@
 # description attributes.
 
 
-class Room:
+from items import Item
 
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
+
+class Room:
+    def __init__(self, room_name, room_description, item):
+        self.room_name = room_name
+        self.room_description = room_description
+        self.item = [item]
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __str__(self):
+        return f"Room: {self.room_name}, Description: {self.room_description}"
+
+    def remove_item(self, item):
+        del self.item[item]
+
+    def add_item(self, item):
+        self.item.append(item)
